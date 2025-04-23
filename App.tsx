843425c9d72864c27890375d744236ec2f040f02
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-04-22 20:24:12 -0400

    resynce Cognito

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, View, StyleSheet } from "react-native";
+import { Amplify } from "aws-amplify";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+import outputs from "./amplify_outputs.json";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+Amplify.configure(outputs);
+
+const formFields = {
+	signUp: {
+		fields: {
+			name: {
+				order: 1,
+				label: "Full Name",
+				placeholder: "Enter your full name",
+				isRequired: true,
+			},
+			email: {
+				order: 2,
+				label: "Email",
+				placeholder: "Enter your email",
+				isRequired: true,
+			},
+			phone_number: {
+				order: 3,
+				label: "Phone Number",
+				placeholder: "Enter your phone number",
+				isRequired: true,
+			},
+			birthdate: {
+				order: 4,
+				label: "Date of Birth",
+				placeholder: "YYYY-MM-DD",
+				isRequired: true,
+			},
+			gender: {
+				order: 5,
+				label: "Gender",
+				placeholder: "Enter your gender identity",
+			},
+			address: {
+				order: 6,
+				label: "Address",
+				placeholder: "Enter your address",
+			},
+			password: {
+				order: 7,
+				label: "Password",
+				placeholder: "Create a strong password",
+				isRequired: true,
+			},
+			confirm_password: {
+				order: 8,
+				label: "Confirm Password",
+				placeholder: "Confirm your password",
+				isRequired: true,
+			},
+		},
+	},
+};
+
+const SignOutButton = () => {
+	const { signOut } = useAuthenticator();
+	return (
+		<View style={styles.signOutButton}>
+			<Button
+				title='Sign Out'
+				onPress={signOut}
+			/>
+		</View>
+	);
+};
+
+const App = () => {
+	return (
+		<Authenticator.Provider>
+			<Authenticator formFields={formFields}>
+				<SignOutButton />
+			</Authenticator>
+		</Authenticator.Provider>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	signOutButton: {
+		alignSelf: "flex-end",
+	},
 });
+
+export default App;
