23bc9a7c610a4b4cb312ca6144e7b9fc6d42f84f
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-04-29 23:13:35 -0400

    Rebuild

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,7 @@
 module.exports = function (api) {
-<<<<<<< HEAD
-	api.cache(true);
-	return {
-		presets: ["babel-preset-expo"],
-		plugins: ["nativewind/babel"],
-	};
-=======
   api.cache(true);
-
   return {
-    presets:  [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+	presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+	plugins: ["nativewind/babel"],
   };
->>>>>>> 6122a210144a24568887ebe85de0e61160bd445d
 };
