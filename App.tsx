cf05e0cf7f8f5fe0fad0d4c5f3451f90cf6b4962
BGreenberg79 <greenbeb2@gmail.com>
2025-05-02 18:04:46 -0400

    struggles with getting Expo 53 and amplify as compatible

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import PatientDashboard from './components/PatientDashboard';
 
 
 export default function App() {
-
   return (
-    <View>
+    <View className="flex-1 bg-gray-700 items-center justify-center">
       <PatientDashboard/>
       <StatusBar style="auto" />
     </View>
