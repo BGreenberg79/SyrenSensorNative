0ecfc497c716e9b5b0b7977ff2770b206d19ffb8
BGreenberg79 <greenbeb2@gmail.com>
2025-05-05 16:24:18 -0400

    improvements to User typing and updates to patient dashboard to make consistent with settings

--- app/patient-dashboard.tsx
+++ app/patient-dashboard.tsx
@@ -1,16 +1,29 @@
 import { View, Text, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useColorScheme } from 'nativewind';
+import { User } from '../models/user';
 
 export default function PatientDashboard() {
-const [patient, setPatient] = useState({
-    firstName: "",
-    lastName: "",
-    gender: "",
+const [patient, setPatient] = useState<User>({
+    userId: 0,
+    name: {
+      firstName: "",
+      lastName: ""
+    },
     age: 0,
+    gender: "",
     height: "",
-    weight: 0,
-    address: ""
+    weight: "",
+    phoneNumber: "",
+    primaryAddress: {
+      buildingNumber: "",
+      street: "",
+      aptUnitNumber: "",
+      zipCode: "",
+      city: "",
+      state: "",
+      country: ""
+    },
 })
 const [vitals, setVitals] = useState({
     skinTemp: 0,
@@ -24,13 +37,25 @@ const [vitals, setVitals] = useState({
 // do for patient information table and for wearable data
 useEffect(()=>{
     setPatient({
-        firstName: "John",
-        lastName: "Doe",
+        userId: Date.now(),
+        name: {
+          firstName: "John",
+          lastName: "Doe"
+        },
         age: 40,
         gender: "male",
         height: "72in",
-        weight: 200,
-        address: "1000 main st"
+        weight: "190 lbs.",
+        phoneNumber: "123-456-7890",
+        primaryAddress: {
+          buildingNumber: "100",
+          street: "main st.",
+          aptUnitNumber: "PH4",
+          zipCode: "12345",
+          city: "Schenectady",
+          state: "NY",
+          country: "United States"
+        },    
     })
     setVitals({
         skinTemp: 98,
@@ -42,24 +67,47 @@ useEffect(()=>{
 
   return (
     <ScrollView className="flex-1 bg-gray-800" contentContainerStyle={{ padding: 20 }}>
-        <Text className="text-white text-3xl font-bold m-2 p-2">
-            Syren Sensor
-        </Text>
+        <View className='flex-1 items-center justify-start px-4 py-6'>
+            <Text className="text-white text-3xl font-bold mb-6">
+                Syren Sensor
+            </Text>
 
-        <View className="bg-black rounded-xl p-6 m-1">
-            <Text className="text-white text-lg font-semibold mb-1">Patient Info:</Text>
-            {Object.entries(patient).map(([key, value]) =>(
-                <Text key={key} className="text-white mb-1 capitalize">
-                    {key}: <Text>{value}</Text>
+            <View className="w-full max-w-md bg-black rounded-2xl p-6 mb-6 shadow-black/40">
+                <Text className="text-white text-lg font-semibold mb-2">Patient Info:</Text>
+                
+                <Text className="text-white b-1 capitalize">
+                    Name: <Text className="font-normal">{`${patient.name.firstName} ${patient.name.lastName}`}</Text>
                 </Text>
-            ))}
 
-            <Text className="text-white text-lg font-semibold mb-1">Patient Vitals:</Text>
-            {Object.entries(vitals).map(([key, value]) =>(
-                <Text key={key} className="text-white mb-1 capitalize">
-                    {key}: <Text>{value}</Text>
-        </Text>
-            ))}
+                <Text className="text-white b-1 capitalize">
+                    Age: <Text className="font-normal">{patient.age}</Text>
+                </Text>
+                
+                <Text className="text-white b-1 capitalize">
+                    Gender: <Text className="font-normal">{patient.gender}</Text>
+                </Text>
+
+                <Text className="text-white b-1 capitalize">
+                    Height: <Text className="font-normal">{patient.height}</Text>
+                </Text>
+
+                <Text className="text-white b-1 capitalize">
+                    Weight: <Text className="font-normal">{patient.weight}</Text>
+                </Text>
+
+                {Object.entries(patient).map(([key, value]) =>(
+                    <Text key={key} className="text-white mb-1 capitalize">
+                        {key}: <Text className='font-normal'>{value}</Text>
+                    </Text>
+                ))}
+
+                <Text className="text-white text-lg font-semibold mt-5 mb-3">Patient Vitals:</Text>
+                {Object.entries(vitals).map(([key, value]) =>(
+                    <Text key={key} className="text-white mb-2 capitalize">
+                        {key}: <Text >{value}</Text>
+            </Text>
+                ))}
+            </View>
         </View>
     </ScrollView>
   )
--- app/settings.tsx
+++ app/settings.tsx
@@ -2,6 +2,7 @@ import { Text, View, ScrollView, TextInput, Button, TouchableOpacity, Alert } fr
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { User } from '../models/user'
 
 export default function settings(){
 
@@ -10,13 +11,16 @@ export default function settings(){
     success: false,
     error: null as  string | null
   })
-  const [user, setUser] = useState({
+  const [user, setUser] = useState<User>({
     userId: 0,
     name: {
       firstName: "",
       lastName: ""
     },
     age: 0,
+    gender: "",
+    height: "",
+    weight: "",
     phoneNumber: "",
     primaryAddress: {
       buildingNumber: "",
@@ -101,123 +105,157 @@ export default function settings(){
 
   return (
     <ScrollView className='flex-1 p-3 bg-gray-800'>
-      <Text className='text-white text-lg font-bold mb-2'>User Info</Text>
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, name: {...user.name, firstName: text} })}
-      value={user.name.firstName}
-      placeholder='First Name'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, name: {...user.name, lastName: text} })}
-      value={user.name.lastName}
-      placeholder='Last Name'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      keyboardType='numeric'
-      onChangeText={(text) => setUser({...user, age: Number(text) })}
-      value={user.age.toString()}
-      placeholder='Age'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, phoneNumber: text })}
-      value={user.phoneNumber}
-      placeholder='Phone Number'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, buildingNumber: text }})}
-      value={user.primaryAddress.buildingNumber}
-      placeholder='Building Number'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, street: text }})}
-      value={user.primaryAddress.street}
-      placeholder='Street'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, aptUnitNumber: text }})}
-      value={user.primaryAddress.aptUnitNumber}
-      placeholder='Apartment/Unit No.'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, zipCode: text }})}
-      value={user.primaryAddress.zipCode}
-      placeholder='Zip/Postal Code'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, city: text }})}
-      value={user.primaryAddress.city}
-      placeholder='City'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, state: text }})}
-      value={user.primaryAddress.state}
-      placeholder='State'
-      />
-      <TextInput 
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, country: text }})}
-      value={user.primaryAddress.country}
-      placeholder='Country'
-      />
-      <Text className='text-white text-lg font-bold mb-2'>Emergency Contact Info</Text>
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setEmergencyContact({...emergencyContact, name: {...emergencyContact.name, firstName: text }})}
-      value={emergencyContact.name.firstName}
-      placeholder='Emergency Contact First Name'
-      />
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setEmergencyContact({...emergencyContact, name: {...emergencyContact.name, lastName: text }})}
-      value={emergencyContact.name.lastName}
-      placeholder='Emergency Contact Last Name'
-      />
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setEmergencyContact({...emergencyContact, phoneNumber: text })}
-      value={emergencyContact.phoneNumber}
-      placeholder='Emergency Contact Phone Number'
-      />
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setEmergencyContact({...emergencyContact, relationship: text })}
-      value={emergencyContact.relationship}
-      placeholder='Emergency Contact Relationship'
-      />
-      <Text className='text-white text-lg font-bold mb-2'>Bluetooth Device Details</Text>
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setBluetoothDevice({...bluetoothDevice, serialNumber: text })}
-      value={bluetoothDevice.serialNumber}
-      placeholder='Device Serial Number'
-      />
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setBluetoothDevice({...bluetoothDevice, deviceMake: text })}
-      value={bluetoothDevice.deviceMake}
-      placeholder='Device Manufacturer/Make'
-      />
-      <TextInput
-      className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
-      onChangeText={(text) => setBluetoothDevice({...bluetoothDevice, deviceModel: text })}
-      value={bluetoothDevice.deviceModel}
-      placeholder='Device Model'
-      />
-      <TouchableOpacity 
-      className='bg-green-600 rounded p-3 mt-4 mb-6'
-      onPress={handleSubmit}>
-        <Text className='text-white text-center font-bold'>Submit</Text>
-      </TouchableOpacity>
+      <View className="flex-1 items-center justify-start px-4 py-6">
+        <View className="w-full max-w-md mb-4">
+          <Text className='text-white text-lg font-bold mb-2'>User Info</Text>
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, name: {...user.name, firstName: text} })}
+          value={user.name.firstName}
+          placeholder='First Name'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, name: {...user.name, lastName: text} })}
+          value={user.name.lastName}
+          placeholder='Last Name'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          keyboardType='numeric'
+          onChangeText={(text) => setUser({...user, age: Number(text) })}
+          value={user.age.toString()}
+          placeholder='Age'
+          />
+          <Text className='text-white text-lg font-bold mb-2'>Gender</Text>
+          <View className='mb-4'>
+            {["Male", "Female", "Non-Binary", "Prefer Not to Say"].map((option) => (
+              <TouchableOpacity
+              key={option}
+              className="flex-row items-center mb-2"
+              onPress={()=> setUser({...user, gender: option})}
+              >
+                <View className={`w-5 h-5 rounded-full border-2 mr-2 ${ user.gender === option ? 'border-blue-500 bg-blue-500' : 'border-gray-400'
+                }`}/>
+                <Text className="text-white">{option}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, weight: text })}
+          value={user.weight}
+          placeholder='Weight'
+          />
+                    <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, height: text })}
+          value={user.height}
+          placeholder='Height'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, phoneNumber: text })}
+          value={user.phoneNumber}
+          placeholder='Phone Number'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, buildingNumber: text }})}
+          value={user.primaryAddress.buildingNumber}
+          placeholder='Building Number'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, street: text }})}
+          value={user.primaryAddress.street}
+          placeholder='Street'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, aptUnitNumber: text }})}
+          value={user.primaryAddress.aptUnitNumber}
+          placeholder='Apartment/Unit No.'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, zipCode: text }})}
+          value={user.primaryAddress.zipCode}
+          placeholder='Zip/Postal Code'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, city: text }})}
+          value={user.primaryAddress.city}
+          placeholder='City'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, state: text }})}
+          value={user.primaryAddress.state}
+          placeholder='State'
+          />
+          <TextInput 
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setUser({...user, primaryAddress: {...user.primaryAddress, country: text }})}
+          value={user.primaryAddress.country}
+          placeholder='Country'
+          />
+          </View>
+        <View className="w-full max-w-md mb-4">
+          <Text className='text-white text-lg font-bold mb-2'>Emergency Contact Info</Text>
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setEmergencyContact({...emergencyContact, name: {...emergencyContact.name, firstName: text }})}
+          value={emergencyContact.name.firstName}
+          placeholder='Emergency Contact First Name'
+          />
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setEmergencyContact({...emergencyContact, name: {...emergencyContact.name, lastName: text }})}
+          value={emergencyContact.name.lastName}
+          placeholder='Emergency Contact Last Name'
+          />
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setEmergencyContact({...emergencyContact, phoneNumber: text })}
+          value={emergencyContact.phoneNumber}
+          placeholder='Emergency Contact Phone Number'
+          />
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setEmergencyContact({...emergencyContact, relationship: text })}
+          value={emergencyContact.relationship}
+          placeholder='Emergency Contact Relationship'
+          />
+        </View>
+        <View className="w-full max-w-md mb-4">
+          <Text className='text-white text-lg font-bold mb-2'>Bluetooth Device Details</Text>
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setBluetoothDevice({...bluetoothDevice, serialNumber: text })}
+          value={bluetoothDevice.serialNumber}
+          placeholder='Device Serial Number'
+          />
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setBluetoothDevice({...bluetoothDevice, deviceMake: text })}
+          value={bluetoothDevice.deviceMake}
+          placeholder='Device Manufacturer/Make'
+          />
+          <TextInput
+          className='bg-gray-700 text-blue-500 p-2 rounded mb-2'
+          onChangeText={(text) => setBluetoothDevice({...bluetoothDevice, deviceModel: text })}
+          value={bluetoothDevice.deviceModel}
+          placeholder='Device Model'
+          />
+        </View>
+        <TouchableOpacity 
+        className='w-full max-w-md bg-green-600 rounded p-3 mt-4 mb-6'
+        onPress={handleSubmit}>
+          <Text className='text-white text-center font-bold'>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   )
 }  
